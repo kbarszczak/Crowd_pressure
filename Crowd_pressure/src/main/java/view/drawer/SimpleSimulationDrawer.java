9aca0c6da6c41b7ebfe9e66c0ac825c67685f95b
kbarszczak <kamilbarszczak62@gmail.com>
2022-11-07 18:13:28 +0100

    Improved color picked in SimpleDrawer class

--- Crowd_pressure/src/main/java/view/drawer/SimpleSimulationDrawer.java
+++ Crowd_pressure/src/main/java/view/drawer/SimpleSimulationDrawer.java
@@ -14,12 +14,9 @@ public class SimpleSimulationDrawer implements SimulationDrawer{
         surface.clearRect(0, 0, surface.getCanvas().getWidth(), surface.getCanvas().getHeight());
 
         for(Agent agent : simulation.getAgents()){
-            if(agent.getVelocity().getValue() < agent.getAgentComfortableSpeed()/3.0) surface.setFill(Color.RED);
-            else if(agent.getVelocity().getValue() < agent.getAgentComfortableSpeed()/2.0) surface.setFill(Color.ORANGE);
-            else surface.setFill(Color.GREEN);
-
             Point position = agent.getPosition();
             double radius = agent.getAgentRadius();
+            surface.setFill(getColor(agent));
             surface.fillOval(position.getX()-radius, position.getY()-radius, 2*radius, 2*radius);
         }
 
@@ -43,5 +40,12 @@ public class SimpleSimulationDrawer implements SimulationDrawer{
         surface.getCanvas().setWidth(surface.getCanvas().getWidth() * scaleX);
         surface.getCanvas().setHeight(surface.getCanvas().getHeight() * scaleY);
         draw(surface, simulation);
+    }
+
+    private Color getColor(Agent agent){
+        double difference = Math.abs(agent.getVelocity().getValue() - agent.getAgentComfortableSpeed()) / (agent.getAgentComfortableSpeed());
+        difference *= 100;
+        if(difference >= 100) difference = 100;
+        return Color.rgb((int)((255*difference)/100), (int)((255 * (100 - difference)) / 100), 0);
     }
 }
